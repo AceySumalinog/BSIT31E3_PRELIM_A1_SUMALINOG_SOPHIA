8d2ddceaacd2033b9726c0400d1030fcdb70e3ec
AceySumalinog <c1982-24@itmlyceumalabang.onmicrosoft.com>
2026-06-17 16:11:45 +0800

    Added basic loop from 1 to 100

---
+++ BSIT31E3_PRELIM_A1_SUMALINOG_SOPHIA.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BSIT31E_PRELIM_A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                // Logic will be added next
+            }
+        }
+    }
+}
